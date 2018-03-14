d3606c71685b2eb2ce78adf411d2f0f9df3bf5dc
JoeyM <joeytermeulen@gmail.com>
2018-03-14 12:27:27 +0100

    Order page is now fully working

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
-    render(){
-        return(
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                };
+                this.setState({ loading: false, orders: fetchedOrders });
+            })
+            .catch(err => {
+                this.setState({ loading: false });
+            })
+    }
+
+    render() {
+        return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id} 
+                        price={parseFloat(order.price).toFixed(2)} 
+                        ingredients={order.ingredients}/>
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
